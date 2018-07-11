367ae5d4538659f7f753f98d0557c64235ce96d2
Thomas Quilliam <rc0028034@student.riversidecollege.ac.uk>
2018-07-11 12:31:44 +0100

    working

--- Assigment2v3/Form1.Designer.cs
+++ Assigment2v3/Form1.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.savePNGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altitudeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.velocityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accelerationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.graphsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveCSVToolStripMenuItem,
+            this.savePNGToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // saveCSVToolStripMenuItem
+            // 
+            this.saveCSVToolStripMenuItem.Name = "saveCSVToolStripMenuItem";
+            this.saveCSVToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveCSVToolStripMenuItem.Text = "Save CSV";
+            this.saveCSVToolStripMenuItem.Click += new System.EventHandler(this.saveCSVToolStripMenuItem_Click_1);
+            // 
+            // savePNGToolStripMenuItem
+            // 
+            this.savePNGToolStripMenuItem.Name = "savePNGToolStripMenuItem";
+            this.savePNGToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.savePNGToolStripMenuItem.Text = "Save PNG";
+            this.savePNGToolStripMenuItem.Click += new System.EventHandler(this.savePNGToolStripMenuItem_Click_1);
+            // 
+            // graphsToolStripMenuItem
+            // 
+            this.graphsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altitudeToolStripMenuItem,
+            this.velocityToolStripMenuItem,
+            this.accelerationToolStripMenuItem});
+            this.graphsToolStripMenuItem.Name = "graphsToolStripMenuItem";
+            this.graphsToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.graphsToolStripMenuItem.Text = "Graphs";
+            // 
+            // altitudeToolStripMenuItem
+            // 
+            this.altitudeToolStripMenuItem.Name = "altitudeToolStripMenuItem";
+            this.altitudeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.altitudeToolStripMenuItem.Text = "Altitude";
+            this.altitudeToolStripMenuItem.Click += new System.EventHandler(this.altitudeToolStripMenuItem_Click_1);
+            // 
+            // velocityToolStripMenuItem
+            // 
+            this.velocityToolStripMenuItem.Name = "velocityToolStripMenuItem";
+            this.velocityToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.velocityToolStripMenuItem.Text = "Velocity";
+            this.velocityToolStripMenuItem.Click += new System.EventHandler(this.velocityToolStripMenuItem_Click);
+            // 
+            // accelerationToolStripMenuItem
+            // 
+            this.accelerationToolStripMenuItem.Name = "accelerationToolStripMenuItem";
+            this.accelerationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.accelerationToolStripMenuItem.Text = "Acceleration";
+            this.accelerationToolStripMenuItem.Click += new System.EventHandler(this.accelerationToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(194, 58);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCSVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem savePNGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graphsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altitudeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem velocityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accelerationToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Assigment2v3/Form1.cs
+++ Assigment2v3/Form1.cs
@@ -7,14 +7,187 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Assigment2v3
 {
     public partial class Form1 : Form
     {
+        class row // this subroutine shows us about are main fields, time, altitude, velocity and acceleration 
+        {
+            public double Time;
+            public double Altitude;
+            public double Velocity;
+            public double Acceleration;
+
+        }
+
+        List<row> table = new List<row>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void CalculateVelocity() // this subroutine shows us how to calculate the velocity, the code uses doubles and strings and integers.
+        {
+            for (int i = 1; i < table.Count; i++)
+            {
+                double dA = table[i].Altitude - table[i - 1].Altitude;
+                double dT = table[i].Time - table[i - 1].Time;
+                table[i].Velocity = dA / dT;
+            }
+        }
+
+        private void CalculateAcceleration() // this subroutine shows us how to calculate the acceleration, this code has doubles doubles, strings and integers.
+        {
+            for (int i = 2; i < table.Count; i++)
+            {
+                double dV = table[i].Velocity - table[i - 1].Velocity;
+                double dT = table[i].Time - table[i - 1].Time;
+                table[i].Acceleration = dV / dT;
+            }
+
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e) // this makes the ability for us to open the csv file. this helps us add data to the program in order to get results 
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "csv files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new row());
+                            string[] r = sr.ReadLine().Split(',');
+                            table.Last().Time = double.Parse(r[0]);
+                            table.Last().Altitude = double.Parse(r[1]);
+
+                        }
+                    }
+                    CalculateVelocity();
+                    CalculateAcceleration();
+                }
+
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "failed to open.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "is not in the required format.");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format.");
+                }
+                catch (DivideByZeroException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " has rows that are the same time.");
+                }
+            }
+
+        }
+
+        private void saveCSVToolStripMenuItem_Click_1(object sender, EventArgs e) // this helps us save the data and produce the charts.
+        {
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.Filter = "csv Files|*.csv";
+            DialogResult results = saveFileDialog1.ShowDialog();
+            if (results == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                    {
+                        sw.WriteLine("Time /s, Altitude /C, Velocity /A, Acceleration / A/s");
+                        foreach (row r in table)
+                        {
+                            sw.WriteLine(r.Time + "," + r.Altitude + "," + r.Velocity + "," + r.Acceleration);
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show(saveFileDialog1.FileName + " failed to save.");
+                }
+            }
+        }
+
+        private void savePNGToolStripMenuItem_Click_1(object sender, EventArgs e) // this code make the graphs from the csv imputs.
+        {
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.Filter = "png Files|*.png";
+            DialogResult results = saveFileDialog1.ShowDialog();
+            if (results == DialogResult.OK)
+            {
+                try
+                {
+                    chart1.SaveImage(saveFileDialog1.FileName, ChartImageFormat.Png);
+                }
+                catch
+                {
+                    MessageBox.Show(saveFileDialog1.FileName + " failed to save.");
+                }
+            }
+        }
+
+        private void altitudeToolStripMenuItem_Click_1(object sender, EventArgs e) // this helps us know the altitude.
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            Series series = new Series
+            {
+                Name = "Altitude",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+            };
+            chart1.Series.Add(series);
+            foreach (row r in table.Skip(1))
+            {
+                series.Points.AddXY(r.Time, r.Altitude);
+            }
+            chart1.ChartAreas[0].AxisX.Title = "time /s";
+            chart1.ChartAreas[0].AxisY.Title = "Altitude /m";
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+        private void accelerationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void velocityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            Series series = new Series
+            {
+                Name = "Velocity",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+            };
+            chart1.Series.Add(series);
+            foreach (row r in table.Skip(1))
+            {
+                series.Points.AddXY(r.Time, r.Velocity);
+            }
+            chart1.ChartAreas[0].AxisX.Title = "time /s";
+            chart1.ChartAreas[0].AxisY.Title = "Velocity /ms^";
+            chart1.ChartAreas[0].RecalculateAxesScale();
         }
     }
 }
